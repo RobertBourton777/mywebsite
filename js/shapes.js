df5398a4bc212da7cd889f4a5a84381d41a03bb5
RobertBourton777 <159321963+RobertBourton777@users.noreply.github.com>
2024-02-12 20:27:02 -0500

    navbar update

---
+++ js/shapes.js
@@ -0,0 +1,6 @@
+const button1 = document.getElementsByClassName('button')[0]
+
+button1.addEventListener('click', () => {
+	alert('You Clicked Me')
+	alert('Notice there is no inline JavaScript')
+})
